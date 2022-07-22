32790ba915ab88de513a76107dbca6a471a6b80a
Maria Luiza Sperancin Mancebo <50486625+malusperancin@users.noreply.github.com>
2022-07-22 10:00:08 -0300

    fix(navigation): corrige comportamento ao trocar de trilho

--- src/components/horizontal-navigation/horizontal-navigation.tsx
+++ src/components/horizontal-navigation/horizontal-navigation.tsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from "react";
 import useNavigation from "../cards/navigation/use-navigation";
 
-function useHorizontalScroll(lenght: number) {
-  const [foco, setFocus] = useState(0);
-  const key = useNavigation();
+export function useHorizontalScroll(lenght: number) {
+    const [foco, setFocus] = useState(0);
+    const key = useNavigation();
+  
+    useEffect(() => {
+      switch (key) {
+        case "LEFT":
+          if (foco !== 0) setFocus(foco - 1);
+          break;
+        case "RIGHT":
+          if (foco !== lenght - 1) setFocus(foco + 1);
+          break;
+        case "UP":
+          setFocus(0);
+          break;
+        case "DOWN":
+          setFocus(0);
+          break;
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [key]);
+  
+    return foco;
+  }
 
-  useEffect(() => {
-    switch (key) {
-      case "LEFT":
-        if (foco !== 0) setFocus(foco - 1);
-        break;
-      case "RIGHT":
-        if (foco !== lenght - 1) setFocus(foco + 1);
-        break;
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [key]);
-
-  return foco;
-}
-
-export default useHorizontalScroll;
